b276d362acd16ebbce4411cd5b02d5f7b4674647
Dipak <dsaha8598@gmail.com>
2025-04-11 12:56:43 +0530

    Added user profile controller.

---
+++ src/main/java/com/careercompass/controller/UserProfileController.java
@@ -0,0 +1,10 @@
+package com.careercompass.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/careerCompass/profile")
+public class UserProfileController {
+
+}
